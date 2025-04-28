2c2b58935d193b61a542670e782143b78349cfee
Ignas <15948796+ignasc@users.noreply.github.com>
2025-04-28 13:54:20 +0100

    Add function to add new book to library.

--- index.js
+++ index.js
@@ -3,54 +3,54 @@ const buttonRead = document.createElement("button");
 const buttonRemove = document.createElement("button");
 const buttonAdd = document.querySelector("#book-add-new");
 const newBookForm = document.querySelector("#book-new-form");
-//const buttonNewBookFormSubmit = document.querySelector("#form-book-submit");
+const buttonNewBookFormSubmit = document.querySelector("#form-book-submit-btn");
 
 const bookLibrary = [
     {
       "id": "2a0cc60c-4160-4430-a0a6-316bcdb1f7d9",
-      "name": "book01",
+      "title": "book01",
       "author": "author01",
       "pages": 123,
       "read": false
     },
     {
       "id": "e84bc301-9006-4dfc-9e7b-13288c57c871",
-      "name": "book02",
+      "title": "book02",
       "author": "author02",
       "pages": 546,
       "read": false
     },
     {
       "id": "11e47826-6bbe-4f82-a314-016ffb6c66dc",
-      "name": "book03",
+      "title": "book03",
       "author": "author03",
       "pages": 274,
       "read": true
     }
   ];
 
-function Book(name, author, pages, read = false) {
+function Book(title, author, pages, read = false) {
     //book constructor
     if (!new.target) {
       throw Error("You must use the 'new' operator to call the constructor");
     }
-    this.name = name;
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 
     return {
         "id": crypto.randomUUID(),
-        "name": this.name,
+        "title": this.title,
         "author": this.author,
         "pages": this.pages,
         "read": this.read,
     }
   };
 
-function addBookToLibrary(name, author, pages, read){
+function addBookToLibrary(title, author, pages, read){
     //add new book to library
-    let newBook = new Book(name, author, pages, read);
+    let newBook = new Book(title, author, pages, read);
     bookLibrary.push(newBook);
 };
 
@@ -72,9 +72,9 @@ function updateBookList(){
             updateBookList();
         });
 
-        newInfo.textContent = "\"" + element["name"] + "\", " + element["author"] + ", pages: " + element["pages"] + ", status: " + (element["read"]?"finished.":"not finished.");
+        newInfo.textContent = "\"" + element["title"] + "\", " + element["author"] + ", pages: " + element["pages"] + ", status: " + (element["read"]?"finished.":"not finished.");
         
-        newButtonRead.textContent = "Mark as read";
+        newButtonRead.textContent = "Update read status";
         newButtonRemove.textContent = "Remove from library";
         
         newItem.setAttribute("id",element["id"]);
@@ -106,11 +106,22 @@ function updateBookReadStatus(id){
     bookLibrary[bookArrayIndex].read = !bookLibrary[bookArrayIndex].read;
 };
 
-function addNewBookToLibrary(){
-    console.log("Add new book: to be implemented");
-};
-
 buttonAdd.addEventListener("click", (e)=>{
     newBookForm.hidden = newBookForm.hidden?false:true;
     e.target.textContent = newBookForm.hidden?"Show new book form":"Hide new book form";
+});
+
+buttonNewBookFormSubmit.addEventListener("click", (e)=>{
+    console.log("form submitted")
+
+    let newFormData = new FormData(newBookForm);
+    let newBookObject = {};
+
+    for(const [key, value] of newFormData){
+        newBookObject[key] = value;
+    };
+    
+    addBookToLibrary(newBookObject.title, newBookObject.author, newBookObject.pages, (newBookObject.read?true:false));
+    updateBookList();
+    e.preventDefault();
 });
